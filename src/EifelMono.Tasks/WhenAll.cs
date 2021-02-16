8413d84f9f6fd62f834d555463def0ab1a09389d
Andreas Klapperich <andreas@klapperich.de>
2021-02-16 11:59:50 +0100

    NEW: Test.cs for new awaitResult classes

--- src/EifelMono.Tasks/WhenAll.cs
+++ src/EifelMono.Tasks/WhenAll.cs
@@ -21,7 +21,6 @@ namespace EifelMono.Tasks
                 return new AwaitStatusTaskWhenAll();
         }
 
-
         public static async Task<AwaitStatusTaskWhenAll<T1, T2>> AwaitStatusAsync<T1, T2>(T1 task1, T2 task2)
             where T1 : Task
             where T2 : Task
@@ -33,8 +32,8 @@ namespace EifelMono.Tasks
                 AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
                 AwaitStatusTasks = new AwaitStatusTask[]
                 {
-                    task1?.AwaitStatusFromTask() ?? new (),
-                    task2?.AwaitStatusFromTask() ?? new ()
+                            task1?.AwaitStatusFromTask() ?? new (),
+                            task2?.AwaitStatusFromTask() ?? new ()
                 },
                 Task1 = task1,
                 Task2 = task2
@@ -53,9 +52,9 @@ namespace EifelMono.Tasks
                 AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
                 AwaitStatusTasks = new AwaitStatusTask[]
                 {
-                    task1?.AwaitStatusFromTask() ?? new (),
-                    task2?.AwaitStatusFromTask() ?? new (),
-                    task3?.AwaitStatusFromTask() ?? new ()
+                            task1?.AwaitStatusFromTask() ?? new (),
+                            task2?.AwaitStatusFromTask() ?? new (),
+                            task3?.AwaitStatusFromTask() ?? new ()
                 },
                 Task1 = task1,
                 Task2 = task2,
@@ -63,168 +62,169 @@ namespace EifelMono.Tasks
             };
         }
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4>> AwaitStatusAsync<T1, T2, T3, T4>(T1 task1, T2 task2, T3 task3, T4 task4)
-             where T1 : Task
-             where T2 : Task
-             where T3 : Task
-             where T4 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync().ConfigureAwait(false);
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4>> AwaitStatusAsync<T1, T2, T3, T4>(T1 task1, T2 task2, T3 task3, T4 task4)
+        //             where T1 : Task
+        //             where T2 : Task
+        //             where T3 : Task
+        //             where T4 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync().ConfigureAwait(false);
 
-            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask() ?? new (),
-                    task2?.AwaitStatusFromTask() ?? new (),
-                    task3?.AwaitStatusFromTask() ?? new (),
-                    task4?.AwaitStatusFromTask() ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2,
-                Task3 = task3,
-                Task4 = task4
-            };
-        }
+        //            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask() ?? new (),
+        //                    task2?.AwaitStatusFromTask() ?? new (),
+        //                    task3?.AwaitStatusFromTask() ?? new (),
+        //                    task4?.AwaitStatusFromTask() ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2,
+        //                Task3 = task3,
+        //                Task4 = task4
+        //            };
+        //        }
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>> AwaitStatusAsync<T1, T2, T3, T4, T5>(T1 task1, T2 task2, T3 task3, T4 task4, T5 task5)
-             where T1 : Task
-             where T2 : Task
-             where T3 : Task
-             where T4 : Task
-             where T5 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync().ConfigureAwait(false);
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>> AwaitStatusAsync<T1, T2, T3, T4, T5>(T1 task1, T2 task2, T3 task3, T4 task4, T5 task5)
+        //             where T1 : Task
+        //             where T2 : Task
+        //             where T3 : Task
+        //             where T4 : Task
+        //             where T5 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync().ConfigureAwait(false);
 
-            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask() ?? new (),
-                    task2?.AwaitStatusFromTask() ?? new (),
-                    task3?.AwaitStatusFromTask() ?? new (),
-                    task4?.AwaitStatusFromTask() ?? new (),
-                    task5?.AwaitStatusFromTask() ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2,
-                Task3 = task3,
-                Task4 = task4,
-                Task5 = task5,
-            };
-        }
+        //            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask() ?? new (),
+        //                    task2?.AwaitStatusFromTask() ?? new (),
+        //                    task3?.AwaitStatusFromTask() ?? new (),
+        //                    task4?.AwaitStatusFromTask() ?? new (),
+        //                    task5?.AwaitStatusFromTask() ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2,
+        //                Task3 = task3,
+        //                Task4 = task4,
+        //                Task5 = task5,
+        //            };
+        //        }
         #endregion
 
-        #region CancellationTokenNode
-        public static async Task<AwaitStatusTaskWhenAll> AwaitStatusAsync(CancellationTokenNode cancellationTokenNode, params Task[] tasks)
-        {
-            var awaitStatusTask = await Task.WhenAll(tasks).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
 
-            if (awaitStatusTask is { })
-                return new AwaitStatusTaskWhenAll
-                {
-                    AwaitStatus = awaitStatusTask.AwaitStatus,
-                    AwaitStatusTasks = tasks.Select(task => task.AwaitStatusFromTask(cancellationTokenNode)).ToArray(),
-                };
-            else
-                return new AwaitStatusTaskWhenAll();
-        }
+        //        #region CancellationTokenNode
+        //        public static async Task<AwaitStatusTaskWhenAll> AwaitStatusAsync(CancellationTokenNode cancellationTokenNode, params Task[] tasks)
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(tasks).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2>> AwaitStatusAsync<T1, T2>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2)
-            where T1 : Task
-            where T2 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
+        //            if (awaitStatusTask is { })
+        //                return new AwaitStatusTaskWhenAll
+        //                {
+        //                    AwaitStatus = awaitStatusTask.AwaitStatus,
+        //                    AwaitStatusTasks = tasks.Select(task => task.AwaitStatusFromTask(cancellationTokenNode)).ToArray(),
+        //                };
+        //            else
+        //                return new AwaitStatusTaskWhenAll();
+        //        }
 
-            return new AwaitStatusTaskWhenAll<T1, T2>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2
-            };
-        }
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2>> AwaitStatusAsync<T1, T2>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2)
+        //            where T1 : Task
+        //            where T2 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3>> AwaitStatusAsync<T1, T2, T3>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3)
-            where T1 : Task
-            where T2 : Task
-            where T3 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
+        //            return new AwaitStatusTaskWhenAll<T1, T2>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2
+        //            };
+        //        }
 
-            return new AwaitStatusTaskWhenAll<T1, T2, T3>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2,
-                Task3 = task3
-            };
-        }
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3>> AwaitStatusAsync<T1, T2, T3>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3)
+        //            where T1 : Task
+        //            where T2 : Task
+        //            where T3 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4>> AwaitStatusAsync<T1, T2, T3, T4>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3, T4 task4)
-             where T1 : Task
-             where T2 : Task
-             where T3 : Task
-             where T4 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
+        //            return new AwaitStatusTaskWhenAll<T1, T2, T3>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2,
+        //                Task3 = task3
+        //            };
+        //        }
 
-            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task4?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2,
-                Task3 = task3,
-                Task4 = task4
-            };
-        }
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4>> AwaitStatusAsync<T1, T2, T3, T4>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3, T4 task4)
+        //             where T1 : Task
+        //             where T2 : Task
+        //             where T3 : Task
+        //             where T4 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
 
-        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>> AwaitStatusAsync<T1, T2, T3, T4, T5>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3, T4 task4, T5 task5)
-             where T1 : Task
-             where T2 : Task
-             where T3 : Task
-             where T4 : Task
-             where T5 : Task
-        {
-            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
+        //            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task4?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2,
+        //                Task3 = task3,
+        //                Task4 = task4
+        //            };
+        //        }
 
-            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>
-            {
-                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
-                AwaitStatusTasks = new AwaitStatusTask[]
-                {
-                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task4?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
-                    task5?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
-                },
-                Task1 = task1,
-                Task2 = task2,
-                Task3 = task3,
-                Task4 = task4,
-                Task5 = task5,
-            };
-        }
-        #endregion
+        //        public static async Task<AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>> AwaitStatusAsync<T1, T2, T3, T4, T5>(CancellationTokenNode cancellationTokenNode, T1 task1, T2 task2, T3 task3, T4 task4, T5 task5)
+        //             where T1 : Task
+        //             where T2 : Task
+        //             where T3 : Task
+        //             where T4 : Task
+        //             where T5 : Task
+        //        {
+        //            var awaitStatusTask = await Task.WhenAll(task1).AwaitStatusAsync(cancellationTokenNode).ConfigureAwait(false);
+
+        //            return new AwaitStatusTaskWhenAll<T1, T2, T3, T4, T5>
+        //            {
+        //                AwaitStatus = awaitStatusTask?.AwaitStatus ?? AwaitStatus.Unknown,
+        //                AwaitStatusTasks = new AwaitStatusTask[]
+        //                {
+        //                    task1?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task2?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task3?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task4?.AwaitStatusFromTask(cancellationTokenNode) ?? new (),
+        //                    task5?.AwaitStatusFromTask(cancellationTokenNode) ?? new ()
+        //                },
+        //                Task1 = task1,
+        //                Task2 = task2,
+        //                Task3 = task3,
+        //                Task4 = task4,
+        //                Task5 = task5,
+        //            };
+        //        }
+        //        #endregion
     }
 }
